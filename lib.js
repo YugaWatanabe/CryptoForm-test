b58c6be5b886c963964f33c3c9638d3365755e4c
YugaWatanabe <yuga.watanabe@wata-lab.meijo-u.ac.jp>
2019-09-30 13:25:11 +0900

    途中までやった分をup

--- lib.js
+++
@@ -1,5 +0,0 @@
-const ipc = require('electron').ipcRenderer;
-ipc.send('test', 'ping');
-ipc.on('test-reply', function (event, arg) {
-    document.getElementById("ret").innerHTML = arg;
-})
--- main.js
+++ main.js
@@ -11,8 +11,12 @@ const ipcMain = electron.ipcMain;
 // ウィンドウを作成するモジュール
 const BrowserWindow = electron.BrowserWindow;
 
+const url = require("url");
+
 // メインウィンドウはGCされないようにグローバル宣言
 let mainWindow = null;
+
+
 
 let loginCallback;
 let subWindow;
@@ -28,15 +32,34 @@ app.on("window-all-closed", () => {
 app.on("ready", () => {
   //ウィンドウサイズを1280*720（フレームサイズを含まない）に設定する
   mainWindow = new BrowserWindow({ width: 1280, height: 720, useContentSize: true });
-  //使用するhtmlファイルを指定する
-  mainWindow.loadFile("index.html");
+  //使用するhtmlファイルを指定する(ローカルのページを表示)
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
+
+  mainWindow.on('app-command', (e, cmd) => {
+    //ブラウザの戻るを検知して処理
+    if (cmd === 'browser-backward' && mainWindow.webContents.canGoBack()) {
+      mainWindow.webContents.goBack();
+    }
+  })
   // ウィンドウが閉じられたらアプリも終了
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
 
 });
+
+/*
+app.on('login', (event, webContents, request, authInfo, callback) => {
+  // これが true なら認証付きプロキシ接続、false なら普通の Basic 認証
+  if (authInfo.isProxy) {
+    event.preventDefault()
+    // 認証用のユーザー名とパスワードを callback に入れればおｋ
+    callback('username', 'password')
+  }
+})
+*/
+
 
 app.on("login", (event, webContents, request, authInfo, callback) => {
   event.preventDefault();
@@ -48,8 +71,14 @@ app.on("login", (event, webContents, request, authInfo, callback) => {
   loginCallback = callback;
 });
 
+
 ipcMain.on("authorization", (event, arg) => {
   console.log(arg);
+
+  loginCallback(arg.username, arg.password);
+  subWindow.close();
+  /*
   subWindow.close();
   loginCallback(arg.username, arg.password);
+  */
 });
